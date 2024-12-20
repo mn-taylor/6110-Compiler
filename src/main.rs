c2882d90a50e4ae022243de095a7ed8b5fe500e0
Yoland Gao <yoland5286@gmail.com>
2024-12-20 15:38:19 -0500

    Initial commit

---
+++ src/main.rs
@@ -0,0 +1,41 @@
+mod add;
+mod utils;
+
+fn get_writer(output: &Option<std::path::PathBuf>) -> Box<dyn std::io::Write> {
+    match output {
+        Some(path) => Box::new(std::fs::File::create(path.as_path()).unwrap()),
+        None => Box::new(std::io::stdout())
+    }
+}
+
+fn main() {
+    let args = utils::cli::parse();
+    let _input = std::fs::read_to_string(&args.input).expect("Filename is incorrect.");
+
+    if args.debug {
+        eprintln!(
+            "Filename: {:?}\nDebug: {:?}\nOptimizations: {:?}\nOutput File: {:?}\nTarget: {:?}",
+            args.input, args.debug, args.opt, args.output, args.target
+        );
+    }
+
+    // Use writeln!(writer, "template string") to write to stdout ot file.
+    let _writer = get_writer(&args.output);
+    match args.target {
+        utils::cli::CompilerAction::Default => {
+            panic!("Invalid target");
+        }
+        utils::cli::CompilerAction::Scan => {
+            todo!("scan");
+        }
+        utils::cli::CompilerAction::Parse => {
+            todo!("parse");
+        }
+        utils::cli::CompilerAction::Inter => {
+            todo!("inter");
+        }
+        utils::cli::CompilerAction::Assembly => {
+            todo!("assembly");
+        }
+    }
+}
