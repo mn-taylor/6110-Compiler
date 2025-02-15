e53ad115a20aff43eba7067cd87a6bf75ab3e0f9
Owen Conoly <otconoly@hotmail.com>
2025-02-15 17:48:31 -0500

    more scanner

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 mod add;
 mod utils;
+const FORM_FEED: char = 12;
 
 fn get_writer(output: &Option<std::path::PathBuf>) -> Box<dyn std::io::Write> {
     match output {
@@ -96,13 +97,133 @@ impl Keyword {
     }
 }
 
+enum Symbol {
+    Plus,
+    Minus,
+    Mul,
+    Div,
+    Mod,
+    Lt,
+    Gt,
+    Leq,
+    Geq,
+    Eq,
+    Neq,
+    And,
+    Or,
+    Not,
+    LPar,
+    RPar,
+    LBrack,
+    RBrack,
+    LBrace,
+    RBrace,
+    Assign,
+    PlusAssign,
+    MinusAssign,
+    MulAssign,
+    DivAssign,
+    ModAssign,
+    PlusPlus,
+    MinusMinus,
+    Semicolon,
+    Comma,
+}
+
+use Symbol::*;
+
+impl Symbol {
+    // not statically checked for completeness :( keep me updated please.
+    fn all() -> Vec<Symbol> {
+        vec![
+            Plus,
+            Minus,
+            Mul,
+            Div,
+            Mod,
+            Lt,
+            Gt,
+            Leq,
+            Geq,
+            Eq,
+            Neq,
+            And,
+            Or,
+            Not,
+            LPar,
+            RPar,
+            LBrack,
+            RBrack,
+            LBrace,
+            RBrace,
+            Assign,
+            PlusAssign,
+            MinusAssign,
+            MulAssign,
+            DivAssign,
+            ModAssign,
+            PlusPlus,
+            MinusMinus,
+            Semicolon,
+            Comma,
+        ]
+    }
+
+    fn to_string(&self) -> &str {
+        match self {
+            Plus => "+",
+            Minus => "-",
+            Mul => "*",
+            Div => "/",
+            Mod => "%",
+            Lt => "<",
+            Gt => ">",
+            Leq => "<=",
+            Geq => ">=",
+            Eq => "==",
+            Neq => "!=",
+            And => "&&",
+            Or => "||",
+            Not => "!",
+            LPar => "(",
+            RPar => ")",
+            LBrack => "[",
+            RBrack => "]",
+            LBrace => "{",
+            RBrace => "}",
+            Assign => "=",
+            PlusAssign => "+=",
+            MinusAssign => "-=",
+            MulAssign => "*=",
+            DivAssign => "/=",
+            ModAssign => "%=",
+            PlusPlus => "++",
+            MinusMinus => "--",
+            Semicolon => ";",
+            Comma => ",",
+        }
+    }
+
+    fn of_string(word: &str) -> Option<Symbol> {
+        for val in Self::all() {
+            if val.to_string() == word {
+                return Some(val);
+            }
+        }
+        None
+    }
+}
+
 enum Token {
     Key(Keyword),
     Ident(String),
+    DecLit(String),
+    HexLit(String),
+    Sym(Symbol),
 }
 
 impl Token {
-    fn of_string(word: String) -> Self {
+    fn of_ident_or_keyword(word: String) -> Self {
         match Keyword::of_string(&word) {
             Some(k) => Self::Key(k),
             None => Self::Ident(word),
@@ -130,13 +251,92 @@ fn substring_before(input: &mut Peekable<Chars>, test: fn(&char) -> bool) -> Str
     return s;
 }
 
-fn scan(input: String) {
+fn scan_char(input: &mut Peekable<Chars>) -> char {
+    let fst = input.next().unwrap(); // TODO error handling
+    match fst {
+        '\\' => {
+            let snd = input.next().unwrap();
+            match snd {
+                '"' => '\"',
+                '\'' => '\'',
+                '\\' => '\\',
+                't' => '\t',
+                'n' => '\n',
+                'r' => '\r',
+                'f' => FORM_FEED,
+                _ => panic!(),
+            }
+        }
+        '\"' => panic!(),
+        '\'' => panic!(),
+        _ => fst,
+    }
+}
+
+fn scan_char_lit(input: &mut Peekable<Chars>) -> char {
+    assert_eq!(input.next(), Some('\''));
+    let ret = scan_char(input);
+    assert_eq!(input.next(), Some('\''));
+    ret
+}
+
+fn scan_str_lit(input: &mut Peekable<Chars>) -> String {
+    let mut ret = String::new();
+    assert_eq!(input.next(), Some('\"'));
+    while input.peek() != Some(&'\"') {
+        ret.push(scan_char(input));
+    }
+    ret
+}
+
+fn scan_dec_lit(input: &mut Peekable<Chars>) -> String {
+    panic!()
+}
+
+fn scan_hex_lit(input: &mut Peekable<Chars>) -> String {
+    panic!()
+}
+
+fn scan_comment(input: &mut Peekable<Chars>) {
+    panic!()
+}
+
+fn scan_block_comment(input: &mut Peekable<Chars>) {
+    panic!()
+}
+
+fn is_alpha(c: char) -> bool {
+    c.is_ascii_alphabetic() || c == '_'
+}
+
+fn is_alphanum(c: char) -> bool {
+    c.is_alpha() || c.is_ascii_digit()
+}
+
+fn scan(input: String) -> Vec<Token> {
     let mut tokens = Vec::new();
     let mut input = input.chars().peekable();
     if let Some(fst) = input.peek() {
         if fst.is_ascii_alphabetic() {
-            let word = substring_before(&mut input, |x| *x == ' ');
-            tokens.push(Token::of_string(word));
+            let word = substring_before(&mut input, |x| !(x.is_ascii_alphabetic() || *x == '_'));
+            tokens.push(Token::of_ident_or_keyword(word));
+        } else if *fst == '0' {
+            let mut input_clone = input.clone();
+            if input_clone.nth(1) == Some('x') {
+                tokens.push(Token::HexLit(scan_hex_lit(&mut input)))
+            } else {
+                tokens.push(Token::DecLit(scan_dec_lit(&mut input)))
+            }
+        } else if *fst == '/' {
+            let mut input_clone = input.clone();
+            if input_clone.nth(1) == Some('/') {
+                scan_comment(&mut input);
+            } else {
+                scan_block_comment(&mut input);
+            }
+        } else if fst.is_ascii_whitespace() {
+            input.next();
         }
     }
+    tokens
 }
