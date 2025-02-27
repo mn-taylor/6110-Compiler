8921043fc9ed384e0940f976ac1dabd31cd3f5bf
Marshall Taylor <mntaylor@mit.edu>
2025-02-26 19:05:20 -0500

    Implemented build_for

--- src/ir_build.rs
+++ src/ir_build.rs
@@ -1,4 +1,5 @@
 use crate::ir::*;
+use crate::parse;
 
 fn build_program(program: parse::Program) {
     // define global scope
@@ -24,4 +25,27 @@ fn build_method(method: parse::Method) {
         params: method.params,
         method_scope: method_scope,
     }
+}
+
+
+fn build_for(ast_for: parse::For, parent_scope: Scope){
+    match ast_for{
+        Stmt::For(withloc_idex, initial_value, condition, location, assignment_expr)=>{
+            let for_scope = Scope{
+                vars: [(withloc_idex, initial_value)],
+                parent: scope
+            };
+
+            let ir_initial_value = build_expr(inital_value);
+            let ir_condition = build_expr(condition);
+            let identifier = build_expr(location);
+            let assigment = build_assign(assignment_expr);
+            let block = build_block(block);
+            
+            return ir::Stmt::For(withloc_idx, inital_value, ir_condition, identifier, assigment, block, for_scope);
+        }
+
+        _=> {}
+    }
+    return None
 }
