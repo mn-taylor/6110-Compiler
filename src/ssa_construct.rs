7db012b632f81db38236e1d1a33223ebe0f2ee1e
Owen Conoly <otconoly@hotmail.com>
2025-04-08 19:52:05 -0400

    dominance frontier alg

--- src/ssa_construct.rs
+++ src/ssa_construct.rs
@@ -7,9 +7,30 @@ fn var_to_def_locs(m: CfgMethod) -> HashMap<VarLabel, HashSet<BlockLabel>> {}
 
 fn dominator_tree(m: CfgMethod) -> HashMap<BlockLabel, HashSet<BlockLabel>> {}
 
-fn dominance_frontier(
+fn dominance_frontiers(
+    g: HashMap<BlockLabel, HashSet<BlockLabel>>,
     dominance_tree: HashMap<BlockLabel, HashSet<BlockLabel>>,
-) -> HashSet<BlockLabel> {
+) -> HashMap<BlockLabel, HashSet<BlockLabel>> {
+    let mut df = HashMap::new();
+    let mut idom = HashMap::new();
+    for (p, children) in dominance_tree.iter() {
+        for c in children {
+            idom.insert(c, *p);
+            df.insert(*c, HashSet::new());
+        }
+    }
+    // Algorithm 3.2 in SSA book
+    for (a, children) in g {
+        for b in children {
+            let mut x = a;
+            // while x does not strictly dominate child
+            while !dominance_tree.get(&x).unwrap().contains(&b) {
+                df.get_mut(&x).unwrap().insert(b);
+                x = *idom.get(&x).unwrap();
+            }
+        }
+    }
+    df
 }
 
 fn insert_phis(m: &mut CfgMethod) {}
