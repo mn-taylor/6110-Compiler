77c5ad546f88035b5f109efa49389856429b6510
Marshall Taylor <mntaylor@mit.edu>
2025-04-26 15:35:32 -0400

    specs for interference graph

---
+++ src/regalloc.rs
@@ -0,0 +1,22 @@
+fn get_defs(m) -> HashMap<VarLabel, HashSet<(block_label, instr_idx)> {
+
+}
+
+fn get_uses(m, block_label, instr_idx) -> HashSet<(block_label, instr_idx)> {
+
+}
+
+fn get_webs(m) -> HashSet<(VarLabel, HashSet<(block_label, instr_idx))>> {
+
+}
+
+fn find_inter_instructions(m, HashSet<(block_label, instr_idx)>) -> {
+
+}
+
+fn interference_graph(m) -> {
+
+}
+
+
+
