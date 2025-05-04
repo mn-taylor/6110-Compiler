8e7e97ae4294147a77e6820c666aaa5cd20e8943
Owen Conoly <otconoly@hotmail.com>
2025-05-03 20:47:58 -0400

    merge plus prune phis

--- src/ssa_construct.rs
+++ src/ssa_construct.rs
@@ -137,15 +137,16 @@ pub fn dominator_tree<T>(
     dom_sets: &HashMap<BlockLabel, HashSet<BlockLabel>>,
 ) -> HashMap<BlockLabel, HashSet<BlockLabel>> {
     let mut dom_tree: HashMap<BlockLabel, HashSet<BlockLabel>> = HashMap::new();
+    let parents = get_parents(&m.blocks);
     let blocks = m.blocks.iter();
 
-    for (label, block) in blocks {
+    for (label, _) in blocks {
         // compute the immediate dominator of the block
 
         let mut agenda = vec![];
-        for parent in block.parents.iter() {
+        for parent in parents.get(label).unwrap() {
             if dom_sets.contains_key(&parent) {
-                agenda.push(*parent);
+                agenda.push(parent);
             }
         }
 
@@ -171,16 +172,16 @@ pub fn dominator_tree<T>(
 
                 new_set.insert(*label);
 
-                dom_tree.insert(curr, new_set.clone());
+                dom_tree.insert(*curr, new_set.clone());
                 break;
             }
 
-            seen.insert(curr);
+            seen.insert(*curr);
 
             // add the new parents
-            for parent in m.blocks.get(&curr).unwrap().parents.iter() {
+            for parent in parents.get(&curr).unwrap() {
                 if dom_sets.contains_key(&parent) {
-                    agenda.push(*parent);
+                    agenda.push(parent);
                 }
             }
         }
@@ -227,9 +228,9 @@ pub fn dominance_frontiers(
 
 pub fn get_graph<T>(m: &mut cfg::CfgMethod<T>) -> HashMap<BlockLabel, HashSet<BlockLabel>> {
     let mut g: HashMap<BlockLabel, HashSet<BlockLabel>> = HashMap::new();
-    get_parents(&mut m.blocks);
+    let parents = get_parents(&mut m.blocks);
     for (label, block) in m.blocks.iter() {
-        if block.parents.len() == 0 && *label != 0 {
+        if parents.get(label).unwrap().len() == 0 && *label != 0 {
             // these nodes are never accessed by the program and confuse the computation of dominance sets
             panic!("ssa construction was given a disconnected graph");
         }
@@ -592,7 +593,7 @@ pub fn rename_variables(
 
     let mut ssa_method: cfg::CfgMethod<SSAVarLabel> = cfg::CfgMethod {
         name: method.name.clone(),
-        params: method.params.clone(),
+        num_params: method.num_params,
         fields: method.fields.clone(),
         blocks: HashMap::new(),
         return_type: method.return_type.clone(),
@@ -650,13 +651,15 @@ pub fn rename_variables(
         }
     }
 
+    let parents = get_parents(&ssa_method.blocks);
     // after the loop, we must still indicate the sources for each of the phi nodes.
-    for block in ssa_method.blocks.values_mut() {
+    for (bid, block) in ssa_method.blocks.iter_mut() {
         for instruction in block.body.iter_mut() {
             match instruction {
                 Instruction::PhiExpr { dest, sources } => {
-                    let new_sources = block
-                        .parents
+                    let new_sources = parents
+                        .get(bid)
+                        .unwrap()
                         .iter()
                         .map(|parent| {
                             (
@@ -752,7 +755,7 @@ pub fn construct(m: &mut CfgMethod) -> cfg::CfgMethod<SSAVarLabel> {
     // Insert Phis
     insert_phis(m);
 
-    // Rename Variable
+    // Rename Variables
     let mut new_method = rename_variables(m, &dominance_tree);
 
     // prune phis: Remove all variables
