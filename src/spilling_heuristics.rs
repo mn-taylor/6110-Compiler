12cfc99f5f146697db11684e7aa0de1585832e75
Marshall Taylor <mntaylor@mit.edu>
2025-05-12 04:46:05 -0400

    thinking of how to dynamically decide how many webs to spill

--- src/spilling_heuristics.rs
+++ src/spilling_heuristics.rs
@@ -13,6 +13,7 @@ type CfgMethod = cfg::CfgMethod<VarLabel>;
 type Jump = cfg::Jump<VarLabel>;
 use crate::regalloc::{find_inter_instructions, Web};
 use std::cmp::max;
+use std::num;
 
 pub fn rank_webs(
     webs: Vec<(u32, &Web)>,
@@ -34,6 +35,21 @@ pub fn rank_webs(
     // webs_and_ratios.reverse();
 
     let sorted_webs = webs_and_ratios.iter().map(|c| *c.0).collect();
+
+    // if let Some((i, min_spilling_cost, web)) = webs_and_ratios.get(0) {
+    //     let mut webs_to_spill = vec![];
+    //     let mut to_skip: HashSet<u32> = hashset! {};
+    //     for (i, spilling_cost, web) in webs_and_ratios.into_iter() {
+    //         // if web is within some percentage of the maximum and is not adjacent to the current webs to spill, then add it to webs to spill
+    //         if (min_spilling_cost - spilling_cost).abs() / min_spilling_cost < 0.1
+    //             && !to_skip.contains(i)
+    //         {
+    //             webs_to_spill.push(i);
+    //             to_skip.extend(interference_graph.get(i).unwrap());
+    //         }
+    //     }
+    // }
+    // // find set of k webs that are not adjacent to each other and have similar spilling cost to the max.
 
     sorted_webs
 }
