39c5c7417d909ae6a27e7b1cb57ebd8a4fb98550
Owen Conoly <otconoly@hotmail.com>
2025-05-13 00:30:07 -0400

    bash

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 chmod +x ./run_1.sh
-( ./run_1.sh "$@" ) & pid=$!
+( ./run_1.sh "$@" -O dumb ) & pid=$!
 ( sleep 100 && kill -HUP $pid ) 2>/dev/null & watcher=$!
 if wait $pid 2>/dev/null; then
     echo "your_command finished"
